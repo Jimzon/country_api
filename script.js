4ca4d66be7199143900071e6f4af71d708f0f194
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-11-22 20:48:13 +0800

    get REST countries

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
+
+const request = new XMLHttpRequest();
+request.open("GET", `https://restcountries.com/v3.1/name/philippines`);
+request.send();
+// console.log(request.responseText);
+
+request.addEventListener("load", function () {
+  const data = JSON.parse(this.responseText);
+  console.log(data);
+});
